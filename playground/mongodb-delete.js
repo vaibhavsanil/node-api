cf3910e9b8493d8f6676bd6a85f1cfccdd98cadd
Vaibhav Sanil <vaibhav.sanil007@gmail.com>
2018-12-17 11:09:16 +0530

    Added delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -22,9 +22,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
 
   //DeleteOne
 
-   db.collection('Todos').deleteOne({text:'Something to Do'}).then((result)=> {
+  /* db.collection('Todos').deleteOne({text:'Something to Do'}).then((result)=> {
    		console.log(result);
-   });
+   });*/
   //FindOndAnd Delete
+
+  db.collection('Users1').findOneAndDelete({_id: new ObjectID("5c166bc8ee15e327135d515f")}).then(
+  	(results) => {
+ 			console.log(JSON.stringify(results,undefined, 2)); 		
+  	});
 	//db.close();
+
 });
