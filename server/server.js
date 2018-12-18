6e1cf9ca2f484a37e34485170a0167af8f2ed776
Vaibhav Sanil <vaibhav.sanil007@gmail.com>
2018-12-18 14:29:20 +0530

    Added test cased get todo s

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 
 
@@ -31,6 +32,26 @@ app.get('/todos',(req, res) =>{
 	},(e)=>{
 		res.status(400).send(e);
 	})
+});
+
+
+app.get('/todos/:id',(req, res) =>{
+	var id = req.params.id
+
+	// Valid id using is valid
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+
 });
 
 app.listen(3000, () => {
