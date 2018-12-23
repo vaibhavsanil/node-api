4150633d4df2f813d31bb2ccfb2fd809c932dadd
Vaibhav Sanil <vaibhav.sanil007@gmail.com>
2018-12-23 12:00:19 +0530

    Added environment variables

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-
+require('./config/config')
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -12,7 +12,9 @@ var {User} = require('./models/user');
 
 
 var app = express();
-const port = process.env.PORT || 3000 ;
+//const port = process.env.PORT || 3000 ;
+const port = process.env.PORT ;
+
 
 app.use(bodyParser.json());
 
