65547d63a4847cf5664cfbb26a0b65246af09bc9
Vaibhav Sanil <vaibhav.sanil007@gmail.com>
2018-12-29 19:41:22 +0530

    Add test for DELETE /user/me/token

--- server/server.js
+++ server/server.js
@@ -169,6 +169,7 @@ app.post('/users/login', (req, res) => {
 });
 
 app.delete('/users/me/token', authenticate,(req,res) => {
+	//console.log(req);
 	req.user.removeToken(req.token).then(() => {
 		res.status(200).send();
 	},() => {
